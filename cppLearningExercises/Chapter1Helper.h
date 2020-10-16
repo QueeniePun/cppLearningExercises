0304a62af05f25601c8856e5c116713c9b22c066
Queenie <queeniep@bu.edu>
2020-10-16 17:49:18 -0400

    Refactor global functions into chapter helper.

---
+++ cppLearningExercises/Chapter1Helper.h
@@ -0,0 +1,26 @@
+#pragma once
+
+class Chapter1Helper
+{
+public:
+    // Constructor
+    Chapter1Helper() {};
+
+    // Destructor 
+    ~Chapter1Helper() {};
+
+    // Method
+    void RunExercise1();
+    void RunExercise2();
+    void RunExercise3();
+    void RunExercise4();
+    void RunExercise5();
+    void RunExercise6();
+    void RunExercise7();
+
+private:
+    void PrintMessage(int num);
+    void PrintSummation(int num);
+
+
+};
