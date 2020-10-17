2662423f8be12702d2abd3c3038e2e39b2a1a5a4
Queenie <queeniep@bu.edu>
2020-10-16 20:56:10 -0400

    Created Chapter3Helper

---
+++ cppLearningExercises/Chapter3Helper.cpp
@@ -0,0 +1,89 @@
+#include "Chapter3Helper.h"
+#include <iostream>
+
+
+using namespace std;
+
+void Chapter3Helper::RunExercise1()
+{
+}
+
+void Chapter3Helper::RunExercise2()
+{
+}
+
+void Chapter3Helper::RunExercise3()
+{
+}
+
+void Chapter3Helper::RunExercise4()
+{
+}
+
+void Chapter3Helper::RunExercise5()
+{
+}
+
+void Chapter3Helper::RunExercise6()
+{
+}
+
+void Chapter3Helper::RunExercise7()
+{
+}
+
+void Chapter3Helper::RunExercise8()
+{
+}
+
+void Chapter3Helper::RunExercise9()
+{
+}
+
+void Chapter3Helper::RunExercise10()
+{
+}
+
+void Chapter3Helper::RunExercise11()
+{
+}
+
+void Chapter3Helper::RunExercise12()
+{
+}
+
+void Chapter3Helper::RunExercise13()
+{
+}
+
+void Chapter3Helper::RunExercise14()
+{
+}
+
+void Chapter3Helper::RunExercise15()
+{
+}
+
+void Chapter3Helper::RunExercise16()
+{
+}
+
+void Chapter3Helper::RunExercise17()
+{
+}
+
+void Chapter3Helper::RunExercise18()
+{
+}
+
+void Chapter3Helper::RunExercise19()
+{
+}
+
+void Chapter3Helper::RunExercise20()
+{
+}
+
+void Chapter3Helper::RunExercise21()
+{
+}
---
+++ cppLearningExercises/Chapter3Helper.h
@@ -0,0 +1,35 @@
+#pragma once
+
+class Chapter3Helper
+{
+public:
+    // Constructor
+    Chapter3Helper() {};
+
+    // Destructor 
+    ~Chapter3Helper() {};
+
+    // Method
+    void RunExercise1();
+    void RunExercise2();
+    void RunExercise3();
+    void RunExercise4();
+    void RunExercise5();
+    void RunExercise6();
+    void RunExercise7();
+    void RunExercise8();
+    void RunExercise9();
+    void RunExercise10();
+    void RunExercise11();
+    void RunExercise12();
+    void RunExercise13();
+    void RunExercise14();
+    void RunExercise15();
+    void RunExercise16();
+    void RunExercise17();
+    void RunExercise18();
+    void RunExercise19();
+    void RunExercise20();
+    void RunExercise21();
+
+};
